c6ca1bda1e35675aa89022fcd74e35140bc47387
arst <artem.startsev@gmail.com>
2025-08-29 10:45:17 +0200

    Move single agent orchestrator to separate project

--- WrtechedWhispers/WretchedWhispers.Infrastructure/Settings.cs
+++ WrtechedWhispers/WretchedWhispers.Infrastructure/Settings.cs
@@ -1,22 +1,16 @@
 using System.Reflection;
 using Microsoft.Extensions.Configuration;
 
-namespace WretchedWhispers.Console;
+namespace WretchedWhispers.Infrastructure;
 
 public class Settings
 {
-    private readonly IConfigurationRoot _configRoot;
+    private readonly IConfigurationRoot _configRoot = new ConfigurationBuilder()
+        .AddEnvironmentVariables()
+        .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
+        .Build();
 
     private AzureOpenAiSettings? _azureOpenAi;
-
-    public Settings()
-    {
-        _configRoot =
-            new ConfigurationBuilder()
-                .AddEnvironmentVariables()
-                .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
-                .Build();
-    }
 
     public AzureOpenAiSettings AzureOpenAi => _azureOpenAi ??= GetSettings<AzureOpenAiSettings>();
 
